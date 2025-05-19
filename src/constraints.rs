b417162e9f65f668aabf0d0c2117101374ab84c4
Saravanan Vijayakumaran <sarva.v@gmail.com>
2025-05-19 16:57:04 +0530

    Fix type errors

--- src/constraints.rs
+++ src/constraints.rs
@@ -1,7 +1,7 @@
 use crate::common::*;
 use ark_bls12_381::Fr;
-use ark_r1cs_std::prelude::*;
-use ark_relations::r1cs::{ConstraintSynthesizer, ConstraintSystemRef, SynthesisError};
+use ark_r1cs_std::{prelude::*, uint64::UInt64};
+use ark_relations::r1cs::{ConstraintSynthesizer, ConstraintSystemRef, Field, SynthesisError};
 use ark_std::vec::Vec;
 
 // round constants for the \iota mapping
@@ -21,47 +21,45 @@ const ROTR: [usize; 25] = [
 ];
 
 // xor_2
-fn xor_2<ConstraintF>(mut cs: ConstraintSystemRef<ConstraintF>, a: &UInt64<ConstraintF>, b: &UInt64<ConstraintF>) -> Result<UInt64<ConstraintF>, SynthesisError>
+fn xor_2<ConstraintF: Field>(a: &UInt64<ConstraintF>, b: &UInt64<ConstraintF>) -> Result<UInt64<ConstraintF>, SynthesisError>
     {
-        // a^b
-        // TODO: implement xor with the UInt64 gadget
-        let xor_2 = UInt64::new_witness(ark_relations::ns!(cs, "xor_2"), || {Ok(a.xor(b))})?;
-        Ok(xor_2)
+        // let xor_2 = UInt64::new_witness(ark_relations::ns!(cs, "xor_2"), || {Ok(a.xor(b))})?;
+        a.xor(b)
     }
 
-// xor_5
-fn xor_5<ConstraintF>(
-    mut cs: ConstraintSystemRef<ConstraintF>,
-    a: &UInt64<ConstraintF>,
-    b: &UInt64<ConstraintF>,
-    c: &UInt64<ConstraintF>,
-    d: &UInt64<ConstraintF>,
-    e: &UInt64<ConstraintF>,
-    ) -> Result<UInt64<ConstraintF>, SynthesisError>
-    {
-        // a^b^c^d^e
-        // TODO: implement xor with the UInt64 gadget
-        let ab = UInt64::new_witness(ark_relations::ns!(cs, "xor_5 first"), || {Ok(a.xor(b))})?;
-        let abc = UInt64::new_witness(ark_relations::ns!(cs, "xor_5 second"), || {Ok(ab.xor(c))})?;
-        let abcd = UInt64::new_witness(ark_relations::ns!(cs, "xor_5 third"), || {Ok(abc.xor(d))})?;
-        let xor_5_result = UInt64::new_witness(ark_relations::ns!(cs, "xor_5 fourth"), || {Ok(abc.xor(e))})?;
-        Ok(xor_5_result)
-    }
+// // xor_5
+// fn xor_5<ConstraintF: Field>(
+//     mut cs: ConstraintSystemRef<ConstraintF>,
+//     a: &UInt64<ConstraintF>,
+//     b: &UInt64<ConstraintF>,
+//     c: &UInt64<ConstraintF>,
+//     d: &UInt64<ConstraintF>,
+//     e: &UInt64<ConstraintF>,
+//     ) -> Result<UInt64<ConstraintF>, SynthesisError>
+//     {
+//         // a^b^c^d^e
+//         // TODO: implement xor with the UInt64 gadget
+//         let ab = UInt64::new_witness(ark_relations::ns!(cs, "xor_5 first"), || {Ok(a.xor(b))})?;
+//         let abc = UInt64::new_witness(ark_relations::ns!(cs, "xor_5 second"), || {Ok(ab.xor(c))})?;
+//         let abcd = UInt64::new_witness(ark_relations::ns!(cs, "xor_5 third"), || {Ok(abc.xor(d))})?;
+//         let xor_5_result = UInt64::new_witness(ark_relations::ns!(cs, "xor_5 fourth"), || {Ok(abc.xor(e))})?;
+//         Ok(xor_5_result)
+//     }
 
-// xor_not_and
-fn xor_not_and<ConstraintF>(
-        mut cs:ConstraintSystemRef<ConstraintF>,
-        a: &UInt64<ConstraintF>,
-        b: &UInt64<ConstraintF>,
-        c: &UInt64<ConstraintF>,
-    ) -> Result<UInt64<ConstraintF>, SynthesisError>
-    {
-        // a^((!b) & c)
-        let nb = b.not(); // TODO: UInt64 gadget for NOT, AND, XOR.
-        let nbc = UInt64:new_witness(ark_relations::ns!(cs,"xor_not_and second"), || {Ok(nb.and(c))})?;
-        let xor_not_and_result = UInt64::new_witness(ark_relations::ns!(cs, "xor_not_and third"), || {Ok(a.xor(nbc))})?;
-        Ok(xor_not_and_result)
-    }
+// // xor_not_and
+// fn xor_not_and<ConstraintF: Field>(
+//         mut cs:ConstraintSystemRef<ConstraintF>,
+//         a: &UInt64<ConstraintF>,
+//         b: &UInt64<ConstraintF>,
+//         c: &UInt64<ConstraintF>,
+//     ) -> Result<UInt64<ConstraintF>, SynthesisError>
+//     {
+//         // a^((!b) & c)
+//         let nb = b.not(); // TODO: UInt64 gadget for NOT, AND, XOR.
+//         let nbc = UInt64::new_witness(ark_relations::ns!(cs,"xor_not_and second"), || {Ok(nb.and(c))})?;
+//         let xor_not_and_result = UInt64::new_witness(ark_relations::ns!(cs, "xor_not_and third"), || {Ok(a.xor(nbc))})?;
+//         Ok(xor_not_and_result)
+//     }
 
 // round_1600
 
